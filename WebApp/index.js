f9265d0e32f8200e1d4ec16204226dff489aa810
Diamondpiggeh <157752935+Diamondpiggeh@users.noreply.github.com>
2024-02-13 01:33:16 -0600

    Most basic Webpage Details added
    page asks for camera permissions and displays camera feed to one of two video frames

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,1 +1,11 @@
 // Included in index.html. Behavioral code and the Metered room management goes here
+let localStream; // local camerad feed and microphone
+let remoteStream; // connected users camera and microphone data
+
+// init function that runs upon first arriving on page
+let init = async() => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) // request permission for camera and audio
+    document.getElementById('user-1').srcObject = localStream
+}
+
+init()
