c492c6253cb6b89c6a0e8aac9a43457545f1cb1b
Jason Xie <xiejason2@gmail.com>
2024-02-22 18:38:34 -0600

    Added Google Stun Server and Audio Tracks The google stun server may not be needed as this might just be for local but will leave it in for now. The audio tracks allow for users to get audio

--- WebApp/index.js
+++ WebApp/index.js
@@ -2,6 +2,18 @@
 let localStream; // local camerad feed and microphone
 let remoteStream; // connected users camera and microphone data
 let peerConnection; // create a peerconnection variable
+
+
+// create free google stunt server - may not need it rn only doing it locally
+const servers = {
+    iceServers:[
+        {
+            urls:['stun:stun1.1.google.com:19302', 'stun:stun2.1.google.com:19302']
+        }
+    ]
+}
+
+
 // init function that runs upon first arriving on page
 let init = async() => {
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) // request permission for camera and audio
@@ -11,10 +23,31 @@ let init = async() => {
 }
 
 let createOffer = async () => {
-    peerConnection = new RTCPeerConnection() // create connection object
+    peerConnection = new RTCPeerConnection(servers) // create connection object
 
     remoteStream = new MediaStream() // setup media stream for user 2
     document.getElementById('user-2').srcObject = remoteStream
+
+    // add audio tracks
+    localStream.getTracks().forEach((track) =>{
+        peerConnection.addTrack(track, localStream)
+    })
+
+    // event listener for remote peer audio tracks and when it adds tracks
+    peerConnection.ontrack = (event) =>{
+        event.streams[0].getTracks().forEach((track)=>{
+            remoteStream.addTrack()
+        })
+    }
+
+
+    // create ice candidate
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            console.log('New ICE candidate:', event.candidate)
+        }
+    }
+
 
     let offer = await peerConnection.createOffer() // create offer
     await peerConnection.setLocalDescription(offer)
