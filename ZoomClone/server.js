a31275918c591bebdfb133d8509205663fe8c50d
Jason Xie <xiejason2@gmail.com>
2024-02-22 19:42:31 -0600

    added code for server

--- ZoomClone/server.js
+++ ZoomClone/server.js
@@ -1,1 +1,8 @@
-console.log('Hi')
+// basic code to get server running
+const express = require('express')
+const app = express()
+const server = require('http').Server(app) // create server based on expresss server
+const io = require('socket.io')(server) // pass server to socket.io so it knows what server to look for
+// note: server is only to setup rooms. video chatting is done through computer
+
+server.listen(3000) // server on port 3000
