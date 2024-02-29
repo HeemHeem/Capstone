16144894b400762306acf16fa57a6e2d304b2648
Jason Xie <xiejason2@gmail.com>
2024-02-29 01:31:45 -0600

    Added Code to mirror camera

--- ZoomClone/server.js
+++ ZoomClone/server.js
@@ -1,11 +1,16 @@
 // basic code to get server running
 const express = require('express')
+
+// Require file system to use local files
+
 // const { Socket } = require('socket.io')
 const app = express()
-const server = require('http').Server(app) // create server based on expresss server
+const server = require('https') // create server based on expresss server
 const io = require('socket.io')(server) // pass server to socket.io so it knows what server to look for
 const {v4: uuidV4} = require('uuid') // remame v4 to uuidV4. this gives a dynamic url
 // note: server is only to setup rooms. video chatting is done through computer
+
+
 
 app.set('view engine', 'ejs') // render views
 app.use(express.static('public')) // set up static folder
@@ -18,6 +23,13 @@ app.get('/:room', (req,res) =>{
     res.render('room', { roomId: req.params.room }) // get room parameter
 })
 
+// Creating object of key and certificate 
+// for SSL 
+const options = { 
+    key: fs.readFileSync("server.key"), 
+    cert: fs.readFileSync("server.cert"), 
+  }; 
+    
 
 io.on('connection', socket => {
     // event for when someone joins a room, pass in room id and user id
@@ -31,5 +43,5 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(3000) // server on port 3000
+server.createServer(options,app).listen(3000) // server on port 3000
 
